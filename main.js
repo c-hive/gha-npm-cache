906e804069268bb8f5c97b611dd22f0b7a4c3809
Csaba Apagyi <csaba.apagyi@gmail.com>
2020-05-21 10:25:29 +0200

    Add hashing

--- main.js
+++ main.js
@@ -1,28 +1,32 @@
 // const core = require("@actions/core");
 const exec = require("@actions/exec");
+const md5File = require("md5-file");
 // const cache = require("@actions/cache");
 // const github = require("@actions/github");
 
 async function run() {
-  let output = "";
-  // let error = "";
+  let os = "";
 
   const options = {};
   options.listeners = {
     stdout: data => {
-      output += data.toString();
+      os += data.toString();
     },
     // stderr: data => {
     //   error += data.toString();
     // },
   };
 
+  await exec.exec("pwd");
+  await exec.exec("ls");
+
   await exec.exec("uname", [], options);
+  const hash = md5File.sync("package-lock.json");
 
   // eslint-disable-next-line no-console
-  console.log(output);
+  console.log(os);
   // eslint-disable-next-line no-console
-  // console.log(error);
+  console.log(hash);
 
   // const cacheKey = await cache.restoreCache(
   //   ["node_modules"],
